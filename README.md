a977f5250b84f0de70b090bc045e8b38f7845ccf
lancebeet <jw@31-208-45-132.cust.bredband2.com>
2018-03-12 00:10:50 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # materialsplot
-Beer-Lambert toy using InteractNext
+Beer-Lambert toy using InteractNext.jl
+
+Data is extracted from the following sites (don't use the csv data for anything important):
+https://www.ncnr.nist.gov/resources/n-lengths/list.html 
+https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
+
---
+++ materialsplot.jl
@@ -0,0 +1,51 @@
+using InteractNext, Plots
+
+const N_A = 6.022e23
+const d = [0:0.05:10...]
+materialdata = readcsv("materialdata.csv")
+# Formatted as
+# name, σ_a, σ_s, ρ, A
+
+gr(show=false)
+
+ui = @manipulate for  
+    search in textbox("search"),
+    element in 1:size(materialdata)[1]
+    
+    σ_astr = materialdata[element, 2]
+    σ_sstr = materialdata[element, 3]
+    σ_a = 0.0
+    σ_s = 0.0
+    try 
+        σ_a = (Float64)(σ_astr)
+    end
+    try
+        σ_s = (Float64)(σ_sstr)
+    end
+    elem = String(materialdata[element,1])
+    ρ = materialdata[element,4]
+    A = materialdata[element,5]
+    N = ρ/A*N_A
+    Σ = N*(σ_s+σ_a)*1e-24
+    Node(:div, Node(:div, elem), 
+         Node(:div, "σ_s = $(σ_sstr)"), 
+         Node(:div, "σ_a = $(σ_astr)"),
+         Node(:div, "A = $A"), 
+         Node(:div, "ρ = $ρ"), 
+         plot(d, exp.(-d*Σ), ylim = (0,1), xlabel = "cm", 
+              ylabel = "Transmission [I/I_0]", label=elem))
+end
+
+on(obs(search)) do val
+    finds = findin([contains(lowercase(i), lowercase(val)) 
+                    for i in materialdata[:,1]], true)
+    if length(far)>=1
+        obs(element)[] = finds[1]
+    end
+end
+
+function responder(req) 
+    ui
+end
+
+webio_serve(page("/", responder), 8004)
